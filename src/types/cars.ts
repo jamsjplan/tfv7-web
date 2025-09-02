2a82cc9817768194a9b4a550b3ff278c94db72d4
Jams Eigyo <jams.jplan04@gmail.com>
2025-09-02 09:18:03 +0900

    fix: CalcResult型の定義

--- src/types/cars.ts
+++ src/types/cars.ts
@@ -14,3 +14,20 @@ export interface CarRow {
   id: number;
   modelId: string;
 }
+
+export interface CalcResult {
+  carPriceTotal: number;
+  taxTotal: number;
+  optionTotal: number;
+  miscTotal: number;
+  totalPurchase: number;
+  tfvTotal: number;
+  leaseBreakdown: Array<{
+    label: string;
+    total: number;
+  }>;
+  resaleCount: number;
+  resaleTotal: number;
+  savings: number;
+  error?: string;
+}
